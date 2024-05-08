ebbf2ffb5bab3e4c0f85d512d27c126e31911b7e
TePee <teven.palmsaar@tktk.ee>
2024-05-08 21:25:28 +0300

    week6arrays

---
+++ week6stringsandarrays/Program.cs
@@ -0,0 +1,27 @@
+﻿string hello = " i see you brother?";
+
+int stringLength = hello.Length;
+
+//string trimmmedString = hello.trim();
+
+hello = hello.Trim();
+
+char firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+Console.WriteLine(hello);
+
+//for(int i = 0; i< hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
